ed05192ff62b0d63e8265dfb570f8948f580e4f7
evolve2122@gmail.com <evolve2122@gmail.com>
2022-09-30 22:26:11 -0500

    Added login access

--- task_0/dashboard/src/App.js
+++ task_0/dashboard/src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import logo from './Holberton-logo.jpg';
+import React from "react";
+import "./App.css";
+import holberton_logo from "./Holberton_logo.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="App-header">
+        <img src={holberton_logo} />
         <h1>School dashboard</h1>
-      </header>
-      
-      <body className="App-body">
+      </div>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
-      </body>
-
-      <footer className="App-footer">
+      </div>
+      <div className="App-footer">
         <p>Copyright 2020 - holberton School</p>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
 

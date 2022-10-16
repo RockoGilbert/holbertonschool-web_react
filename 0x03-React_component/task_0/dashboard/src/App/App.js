123d0e9d486e9f70ca5a451153219b09a8568d8d
evolve2122@gmail.com <evolve2122@gmail.com>
2022-10-16 09:30:33 -0500

    Converted the App Function to a React Class

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 import Notifications from '../Notifications/Notifications'
-import Header from '../Header/Header'
-import Login from '../Login/Login'
-import CourseList from '../CourseList/CourseList'
-import Footer from '../Footer/Footer'
-import PropTypes from 'prop-types'; // ES6
-import { getLatestNotification } from '../utils/utils'
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
 
-eslint-disable jest/valid-expect;
+
+// array for the list of courses
 const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 }
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40}
 ];
 
+// array for the list of notifications
 const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent', value: 'New resume available'},
+  {id: 3, type: 'urgent', html: {__html: getLatestNotification()}}
 ];
-
 class App extends React.Component {
+  componentDidMount() {
+    document.addEventListener("keydown", (e) => {
+      if (e.ctrlKey && e.key === 'h') {
+        // e.preventDefault();
+        alert("Logging you out");
+        this.props.logOut();
+      }
+    });
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", (e) => {});
+  }
+
+
   render() {
+    const { isLoggedIn } = this.props;
     return (
       <React.Fragment>
-        <Notifications listNotifications={listNotifications}></Notifications>
-        <div className='App'>
-          <Header></Header>
-          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} ></CourseList> : <Login></Login>}
-          <Footer></Footer>
+        <Notifications  listNotifications={listNotifications}/>
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            {isLoggedIn ? <CourseList  listCourses={listCourses} /> : <Login />}
+          </div>
+          <div className="App-footer">
+            <Footer />
+          </div>
         </div>
       </React.Fragment>
     );
@@ -36,11 +57,13 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
 };
 
 App.defaultProps = {
-  isLoggedIn: false
+  isLoggedIn: false,
+  logOut: () => {console.log("Logging you out")},
 };
 
 export default App;

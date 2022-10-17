ea88b834d2f6118efc3682590bd3d0ecbb3d24d8
evolve2122@gmail.com <evolve2122@gmail.com>
2022-10-17 09:13:59 -0500

    Edited Readme

--- 0x00-react_state/task_0/dashboard/src/Header/Header.js
+++ 0x00-react_state/task_0/dashboard/src/Header/Header.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import logo from '../assets/holberton-logo.jpg';
+import { AppContext } from '../App/AppContext';
 
 // Definition of styles
 const styles = StyleSheet.create({
@@ -17,21 +18,54 @@ const styles = StyleSheet.create({
       width: '15rem',
     },
   },
-  title: {
-    display: 'inline',
-    fontSize: '1.5rem',
-    '@media (min-width: 900px)': {
-      fontSize: '2rem',
-      marginLeft: '1rem',
+  titleDiv: {
+    flexDirection: 'column',
+    marginLeft: '2rem',
+    textAlign: 'left',
+    color: 'black',
+    ':nth-child(1n) > h1': {
+      fontSize: '1.5rem',
+      '@media (min-width: 900px)': {
+        fontSize: '2rem',
+      },
     },
+  },
+  button: {
+    border: 'none',
+    background: 'none',
+    fontSize: '1rem',
   },
 });
 
-export default function Header() {
+const Header = function Header() {
+  // Take values from context - defined in other components
+  const { user, logOut } = useContext(AppContext);
+
   return (
     <header className={css(styles.AppHeader)}>
       <img className={css(styles.imgSize)} src={logo} alt="logo" />
-      <h1 className={css(styles.title)}>School dashboard</h1>
+      <div className={css(styles.titleDiv)}>
+        <h1>School dashboard</h1>
+        {user.isLoggedIn ? (
+          <div id="logoutSection">
+            <p>
+              Welcome
+              {' '}
+              {user.email}
+              {' '}
+              <button
+                type="button"
+                className={css(styles.button)}
+                onClick={logOut}
+              >
+                (logout)
+              </button>
+            </p>
+          </div>
+        ) : null}
+      </div>
     </header>
   );
-}
+};
+
+export default Header;
--- 0x00-react_state/task_0/dashboard/src/Header/Header.test.js
+++ 0x00-react_state/task_0/dashboard/src/Header/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { StyleSheetTestUtils } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 import Header from './Header';
 
 describe('Header component', () => {
@@ -25,5 +26,67 @@ describe('Header component', () => {
   it('Verifies that h1 tag rendered', () => {
     const wrapper = shallow(<Header />);
     expect(wrapper.find('h1').length).toEqual(1);
+  });
+
+  // Logged in vs not logged in
+  it('Verifies that renders correctly when mounted with default context', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('#logoutSection').length).toEqual(0);
+  });
+
+  it('Verifies that renders correctly when mounted with not logged in context', () => {
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    const context = {
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+      logOut: jest.fn(),
+    };
+    const wrapper = mount(
+      <AppContext.Provider value={context}>
+        <Header />
+      </AppContext.Provider>,
+    );
+    expect(wrapper.find('#logoutSection').length).toEqual(0);
+  });
+
+  it('Verifies that renders correctly when mounted with logged in context', () => {
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    const context = {
+      user: {
+        email: 'email@email.com',
+        password: 'password',
+        isLoggedIn: true,
+      },
+      logOut: jest.fn(),
+    };
+    const wrapper = mount(
+      <AppContext.Provider value={context}>
+        <Header />
+      </AppContext.Provider>,
+    );
+    expect(wrapper.find('#logoutSection').length).toEqual(1);
+    expect(wrapper.find('#logoutSection').text()).toEqual('Welcome email@email.com (logout)');
+  });
+
+  it('Verifies that logs out when button is clicked', () => {
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    const context = {
+      user: {
+        email: 'email@email.com',
+        password: 'password',
+        isLoggedIn: true,
+      },
+      logOut: jest.fn(),
+    };
+    const wrapper = mount(
+      <AppContext.Provider value={context}>
+        <Header />
+      </AppContext.Provider>,
+    );
+    wrapper.find('button').simulate('click');
+    expect(context.logOut).toHaveBeenCalled();
   });
 });
